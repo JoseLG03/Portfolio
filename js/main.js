164c291783a56d31408b10f5a7d9c220ea44804a
JoseLG03 <antares069@gmail.com>
2020-10-21 20:53:54 -0500

    agregando la funcion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let logo_face = document.getElementById( 'logo_face' );
 let logo_github = document.getElementById( 'logo_github' );
+let logo_linkedin = document.getElementById( 'logo_linkedin' );
+let logo_whats = document.getElementById('logo_whats');
 
 //funcion para cambiar el src de la imagen del logo
 function cambiarImagen(id, img){
@@ -7,13 +9,12 @@ function cambiarImagen(id, img){
 }
 
 logo_face.onmouseover = function(){
-  cambiarImagen('logo_face', "./img/facebook_azul.png");
+  cambiarImagen('logo_face', './img/facebook_azul.png');
 }  
 
-logo_face.onmouseover = function(){
-  cambiarImagen('logo_face', "./img/facebook_negro.png");
+logo_face.onmouseleave = function(){
+  cambiarImagen('logo_face', './img/facebook_negro.png');
 }  
-
 
 logo_github.onmouseover = function(){
   cambiarImagen('logo_github', './img/github_azul.png');
@@ -21,6 +22,22 @@ logo_github.onmouseover = function(){
 
 logo_github.onmouseleave = function(){
   cambiarImagen('logo_github', './img/github_negro.png');
+}   
+
+logo_linkedin.onmouseover = function(){
+  cambiarImagen('logo_linkedin', './img/linkedin_azul.png');
+}  
+
+logo_linkedin.onmouseleave = function(){
+  cambiarImagen('logo_linkedin', './img/linkedin_negro.png');
+}   
+
+logo_whats.onmouseover = function(){
+  cambiarImagen('logo_whats', './img/whatsapp_verde.png');
+}  
+
+logo_whats.onmouseleave = function(){
+  cambiarImagen('logo_whats', './img/whatsapp_negro.png');
 }   
 
   
