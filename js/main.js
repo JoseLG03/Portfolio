f17ac985856ebd7ab8accef88f1bbe358f73eff4
JoseLG03 <antares069@gmail.com>
2020-11-10 13:16:02 -0600

    validacion al form

--- js/main.js
+++ js/main.js
@@ -40,3 +40,39 @@ logo_whats.onmouseleave = function(){
   cambiarImagen(logo_whats.id, './img/whatsapp_negro.png');
 }   
 
+//valition form
+var namePattern = "^[a-z A-Z]{4,30}$";
+var emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$";
+
+function checkInput(idInput, pattern) {
+	return $(idInput).val().match(pattern) ? true : false;
+}
+
+function checkTextarea(idText) {
+	return $(idText).val().length > 12 ? true : false;	
+}
+
+function enableSubmit (idForm) {
+	$(idForm + " button.submit").removeAttr("disabled");
+}
+
+function disableSubmit (idForm) {
+	$(idForm + " button.submit").attr("disabled", "disabled");
+}
+
+function checkForm (idForm) {
+	$(idForm + " *").on("change keydown", function() {
+		if (checkInput("#nombre", namePattern) &&  
+      checkInput("#email", emailPattern) &&
+      checkTextarea("#mensaje"))
+		{
+			enableSubmit(idForm);
+		} else {
+			disableSubmit(idForm);
+		}
+	});
+}
+
+$(function() {
+	checkForm("#contacto");
+});
