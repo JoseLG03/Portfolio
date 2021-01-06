430954863dd16d1dd94e8f284ffc9cbcc3b4e843
JoseLG03 <antares069@gmail.com>
2021-01-05 22:16:22 -0600

    script de correo

--- sendmail.php
+++ sendmail.php
@@ -1,13 +1,7 @@
 <?php
-if(isset($_POST['email'])) {
 
 $email_to = "contact@codingwithpepe.com";
 $email_subject = "Contacto desde mi sitio web";
-
-echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-die();
-}
 
 $email_message = "Detalles del formulario de contacto:\n\n";
 $email_message .= "Nombre: " . $_POST['nombre'] . "\n";
@@ -19,5 +13,4 @@ $headers = 'From: '.$email_from."\r\n".
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 
-echo "¡El formulario se ha enviado con éxito!";
 ?>
